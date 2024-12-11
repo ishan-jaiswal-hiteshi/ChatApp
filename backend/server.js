81f9d5cedb1143d107976032782408239b35384a
ishan jaiswal <ishan.jaiswal@hiteshi.com>
2024-12-11 11:12:09 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -123,7 +123,7 @@ sequelize
     console.error("Error creating tables: ", error);
   });
 
-// Middleware to authenticate JWT
+// authenticate JWT
 const authenticateJWT = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "Unauthorized" });
